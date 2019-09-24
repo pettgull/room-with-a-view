59e1e344d288612781bad6cb50c082045b68e463
Andre Lauritzen <lauritzen.42@gmail.com>
2019-09-24 16:38:02 +0200

    Create a location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,17 +1,4 @@
 class LocationsController < ApplicationController
-  def new
-    @location = Location.new
-  end
-
-  def create
-    @location = Location.new
-    if @location.save
-      # redirect_to location_path(@location)
-    else
-      render 'new'
-    end
-  end
-
   def index
     @locations = Location.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :locations, only: [:show, :index]
+  resources :locations, only: [:show, :index, :new]
   resources :bookings, only: [:index]
   namespace :profile do
     resources :locations, except: [:show, :index]
