1e6e047b740e536436e49cfca721d465a300893b
petter gulliksen <petter-gulliksen@hotmail.com>
2019-09-24 13:43:31 +0200

    did the necessary validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :location
+  validates :rating, presence: true, numericality: true
+  validates :description, presence: true
+  validates :date, presence: true
 end
