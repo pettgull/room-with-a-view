5ec483330c33b907de689bf3da941befb1b2320f
magnus berntsen <magnber@gmail.com>
2019-09-27 11:14:11 +0200

    refactored map lat_lng function and adjusted navbar and searchform

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,19 +1,23 @@
 class LocationsController < ApplicationController
-
+  before_action :set_location, only: [:set_lat_lng, :show]
   def index
     if params[:query].present?
       @locations = Location.where("address ILIKE ?", "%#{params[:query]}%").geocoded
     else
       @locations = Location.geocoded
     end
+    set_lat_lng
+  end
 
+  def show
+    @locations = Location.geocoded.where(id: @location)
+    @booking = Booking.new
+    set_lat_lng
+  end
 
+  private
 
-
-
-    # @locations = Location.geocoded
-    # # @locations = Location.all
-
+  def set_lat_lng
     @markers = @locations.map do |location|
       {
         lat: location.latitude,
@@ -24,19 +28,7 @@ class LocationsController < ApplicationController
     end
   end
 
-  def show
+  def set_location
     @location = Location.find(params[:id])
-    @locations = Location.geocoded.where(id: @location)
-
-    @booking = Booking.new
-
-    @markers = @locations.map do |location|
-      {
-        lat: location.latitude,
-        lng: location.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { location: location }),
-        image_url: helpers.asset_url('home-solid.svg')
-      }
-    end
   end
 end
